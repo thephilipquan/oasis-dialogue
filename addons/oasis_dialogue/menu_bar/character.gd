91c5e37b0c2995547954a9eb1f8d3f7b909f2d90
thephilipquan <thephilipquan@gmail.com>
2025-10-30 21:50:28 -0700

    feat: disable menu items when appropriate

--- addons/oasis_dialogue/menu_bar/character.gd
+++ addons/oasis_dialogue/menu_bar/character.gd
@@ -3,12 +3,25 @@ extends PopupMenu
 
 const REGISTRY_KEY := "character_menu"
 
+const _AddCharacter := preload("res://addons/oasis_dialogue/canvas/add_character_handler.gd")
+const _ProjectManager := preload("res://addons/oasis_dialogue/main/project_manager.gd")
+const _RemoveCharacter := preload("res://addons/oasis_dialogue/canvas/remove_character_handler.gd")
 const _Registry := preload("res://addons/oasis_dialogue/registry.gd")
 
 signal new_character_requested
 signal save_requested
 signal rename_requested
 signal remove_requested
+
+enum Item {
+	NEW,
+	SAVE,
+	RENAME,
+	# Separator here consumes an id.
+	REMOVE = RENAME + 2,
+}
+
+var _get_active_character := Callable()
 
 
 func _ready() -> void:
@@ -24,29 +37,65 @@ func set_items() -> void:
 
 	i.ctrl_pressed = true
 	i.keycode = KEY_N
-	add_item("New...", -1, i.get_keycode_with_modifiers())
+	add_item("New...", Item.NEW, i.get_keycode_with_modifiers())
 
 	i = InputEventKey.new()
 	i.ctrl_pressed = true
 	i.keycode = KEY_S
-	add_item("Save", -1, i.get_keycode_with_modifiers())
+	add_item("Save", Item.SAVE, i.get_keycode_with_modifiers())
 
-	add_item("Rename")
+	add_item("Rename", Item.RENAME)
 
 	add_separator()
-	add_item("Delete")
+	add_item("Delete", Item.REMOVE)
 
 
 func register(registry: _Registry) -> void:
 	registry.add(REGISTRY_KEY, self)
 
 
-func emit_item_signal(id: int) -> void:
-	var signals: Array[Signal] = [
-			new_character_requested,
-			save_requested,
-			rename_requested,
-			Signal(), # Separator.
-			remove_requested,
+func setup(registry: _Registry) -> void:
+	var project_manager: _ProjectManager = registry.at(_ProjectManager.REGISTRY_KEY)
+	init_get_active_character(project_manager.get_active_character)
+
+	var add_character: _AddCharacter = registry.at(_AddCharacter.REGISTRY_KEY)
+	add_character.character_added.connect(update_item_states.unbind(1), CONNECT_DEFERRED)
+
+	var remove_character: _RemoveCharacter = registry.at(_RemoveCharacter.REGISTRY_KEY)
+	remove_character.character_removed.connect(update_item_states, CONNECT_DEFERRED)
+
+	project_manager.project_loaded.connect(update_item_states, CONNECT_DEFERRED)
+	project_manager.character_loaded.connect(update_item_states.unbind(1), CONNECT_DEFERRED)
+
+
+func init_get_active_character(callback: Callable) -> void:
+	_get_active_character = callback
+
+
+func update_item_states() -> void:
+	const items: Array[Item] = [
+			Item.SAVE,
+			Item.RENAME,
+			Item.REMOVE,
 	]
-	signals[id].emit()
+	var active_character: String = _get_active_character.call()
+	var callback := Callable()
+	if active_character:
+		callback = set_item_disabled.bind(false)
+	else:
+		callback = set_item_disabled.bind(true)
+	items.map(callback)
+
+
+func emit_item_signal(id: int) -> void:
+	var s := Signal()
+	match id:
+		Item.NEW:
+			s = new_character_requested
+		Item.SAVE:
+			s = save_requested
+		Item.RENAME:
+			s = rename_requested
+		Item.REMOVE:
+			s = remove_requested
+	s.emit()
--- addons/oasis_dialogue/menu_bar/project.gd
+++ addons/oasis_dialogue/menu_bar/project.gd
@@ -3,10 +3,21 @@ extends PopupMenu
 
 const REGISTRY_KEY := "project_menu"
 
+const _AddCharacter := preload("res://addons/oasis_dialogue/canvas/add_character_handler.gd")
+const _CharacterTree := preload("res://addons/oasis_dialogue/canvas/character_tree.gd")
+const _ProjectManager := preload("res://addons/oasis_dialogue/main/project_manager.gd")
+const _RemoveCharacter := preload("res://addons/oasis_dialogue/canvas/remove_character_handler.gd")
 const _Registry := preload("res://addons/oasis_dialogue/registry.gd")
 
 signal save_requested
 signal export_requested
+
+enum Item {
+	SAVE = 0,
+	EXPORT = 1,
+}
+
+var _get_character_count := Callable()
 
 
 func _ready() -> void:
@@ -22,12 +33,38 @@ func set_items() -> void:
 	i.ctrl_pressed = true
 	i.shift_pressed = true
 	i.keycode = KEY_S
-	add_item("Save", -1, i.get_keycode_with_modifiers())
-	add_item("Export")
+	add_item("Save", Item.SAVE, i.get_keycode_with_modifiers())
+	add_item("Export", Item.EXPORT)
 
 
 func register(registry: _Registry) -> void:
 	registry.add(REGISTRY_KEY, self)
+
+
+func setup(registry: _Registry) -> void:
+	var tree: _CharacterTree = registry.at(_CharacterTree.REGISTRY_KEY)
+	init_get_character_count(tree.get_item_count)
+
+	var add_character: _AddCharacter = registry.at(_AddCharacter.REGISTRY_KEY)
+	add_character.character_added.connect(update_item_states.unbind(1), CONNECT_DEFERRED)
+
+	var remove_character: _RemoveCharacter = registry.at(_RemoveCharacter.REGISTRY_KEY)
+	remove_character.character_removed.connect(update_item_states, CONNECT_DEFERRED)
+
+	var project_manager: _ProjectManager = registry.at(_ProjectManager.REGISTRY_KEY)
+	project_manager.project_loaded.connect(update_item_states, CONNECT_DEFERRED)
+
+
+func init_get_character_count(callback: Callable) -> void:
+	_get_character_count = callback
+
+
+func update_item_states() -> void:
+	var character_count: int = _get_character_count.call()
+	if character_count == 0:
+		set_item_disabled(Item.EXPORT, true)
+	else:
+		set_item_disabled(Item.EXPORT, false)
 
 
 func emit_item_signal(id: int) -> void:
