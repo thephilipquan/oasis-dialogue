212d53d418f242c63a2f74e0c4ae44e7f1c861d8
thephilipquan <thephilipquan@gmail.com>
2025-11-10 15:16:35 -0800

    feat(node_utils): add find_type

--- addons/oasis_dialogue/utils/node_utils.gd
+++ addons/oasis_dialogue/utils/node_utils.gd
@@ -6,3 +6,17 @@ static func call_group_under_parent(parent: Node, group: StringName, method: Str
 		if parent.is_ancestor_of(node):
 			if method in node:
 				node.callv(method, arguments)
+
+
+static func find_type(root: Node, type: Variant) -> Node:
+	var queue: Array[Node] = [root]
+	var result: Node = null
+	while queue and not result:
+		var current: Node = queue.pop_front()
+		for child in current.get_children():
+			if is_instance_of(child, OasisManager):
+				result = child
+				break
+			else:
+				queue.push_back(child)
+	return result
