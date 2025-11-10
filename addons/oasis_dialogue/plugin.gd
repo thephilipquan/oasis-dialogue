bde7c12678a62ad45368037319bd474ed435693f
thephilipquan <thephilipquan@gmail.com>
2025-11-09 20:51:30 -0800

    fix: plugin correctly instantiates scene

--- addons/oasis_dialogue/plugin.gd
+++ addons/oasis_dialogue/plugin.gd
@@ -2,12 +2,13 @@
 extends EditorPlugin
 
 const _App := preload("res://addons/oasis_dialogue/main/main.gd")
+const _AppScene := preload("res://addons/oasis_dialogue/main/main.tscn")
 
 var _app: _App = null
 
 
 func _enter_tree() -> void:
-	_app = _App.new()
+	_app = _AppScene.instantiate()
 	add_control_to_bottom_panel(_app, "Dialogue")
 
 
