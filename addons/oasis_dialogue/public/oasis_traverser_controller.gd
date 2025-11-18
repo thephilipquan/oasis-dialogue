e743310839f7bfbce2b949aa06883b8566c4d2c5
thephilipquan <thephilipquan@gmail.com>
2025-11-18 01:04:24 -0800

    refactor(traverser_controller): remove unused virtual methods

--- addons/oasis_dialogue/public/oasis_traverser_controller.gd
+++ addons/oasis_dialogue/public/oasis_traverser_controller.gd
@@ -5,17 +5,11 @@ extends Node
 @abstract
 func get_annotation() -> String
 
-func reachable_branches_set(traverser: OasisTraverser) -> void:
-	pass
-
 func has_prompt(traverser: OasisTraverser) -> bool:
 	return false
 
 func increment_prompt_index(traverser: OasisTraverser) -> bool:
 	return false
-
-func start(traverser: OasisTraverser) -> void:
-	pass
 
 func finish(traverser: OasisTraverser) -> void:
 	pass
