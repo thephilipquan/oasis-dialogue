e1e8eb90c19854d256ea85c15ff20cda40061ced
thephilipquan <thephilipquan@gmail.com>
2025-12-04 16:59:45 -0800

    feat(string_utils): add is_alpha

--- addons/oasis_dialogue/utils/string_utils.gd
+++ addons/oasis_dialogue/utils/string_utils.gd
@@ -8,3 +8,13 @@ static func replace_extension(s: String, with: String) -> String:
 	var result := s.trim_suffix(".%s" % s.get_extension())
 	result += ".%s" % with
 	return result
+
+
+static func is_alpha(s: String) -> bool:
+	s = s.to_lower()
+	var result := true
+	for l in s:
+		if l < "a" or l > "z":
+			result = false
+			break
+	return result
