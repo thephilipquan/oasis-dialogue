0b3d722b3d2016df09fa3cd2e9949c2776e348bf
thephilipquan <thephilipquan@gmail.com>
2026-01-05 16:40:20 -0800

    refactor: move plugin to godot's main window

--- addons/oasis_dialogue/plugin.gd
+++ addons/oasis_dialogue/plugin.gd
@@ -9,9 +9,25 @@ var _app: _App = null
 
 func _enter_tree() -> void:
 	_app = _AppScene.instantiate()
-	add_control_to_bottom_panel(_app, "Dialogue")
+	EditorInterface.get_editor_main_screen().add_child(_app)
+	_app.hide()
 
 
 func _exit_tree() -> void:
-	remove_control_from_bottom_panel(_app)
 	_app.queue_free()
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	_app.visible = visible
+
+
+func _get_plugin_name():
+	return "OasisDialogue"
+
+
+func _get_plugin_icon():
+	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
